66ee3ade8a0190f89bc1c9ae4dd4b7395ff408f0
alexcbrady <114630867+alexcbrady@users.noreply.github.com>
2023-12-17 16:45:10 -0500

    GUI

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ from argparse import ArgumentParser
 from typing import *
 import pysrt
 import datetime
+import tkinter as tk
+import webbrowser
+from tkinter import scrolledtext as st
+
 
 load_dotenv()
 
@@ -37,6 +41,8 @@ class logger:
     toSRT = f'LOGGER: {nowTime} : CONVERTING TEXT TO SRT'
     subtitleCreate = f'LOGGER: {nowTime} : CREATING SUBTITLES'
     subtitleVieoMerge = f'LOGGER: {nowTime} : MERGING VIDEO WITH SUBTITLES'
+    buildingVideo = f'Logger: {nowTime} : BUILDING VIDEO'
+    writingVideo = f'Logger: {nowTime} : WRITING VIDEO'
 
 log = logger()
 
@@ -50,14 +56,14 @@ def download_youtube_video(link: str) -> None:
     
     yt.streams.filter(file_extension='mp4').first().download(f'{SAVE_PATH}')
 
-def get_reddit_posts(subreddit: str):
+def get_reddit_posts(subreddit: str, sliderNum):
     target = reddit.subreddit(subreddit)
     print(f'Display Name:{target.display_name}')
-    return target.top("month")
+    return target.top(limit=sliderNum)
 
 
 def merge_video_audio(video_file_path: str, audio_file_path: str) -> VideoFileClip:
-    print(log.audioVideoMerge)
+    updateLogger(log.audioVideoMerge)
     video_clip = VideoFileClip(video_file_path)
     audio_clip = AudioFileClip(audio_file_path)
     audio_length = audio_clip.duration
@@ -66,7 +72,7 @@ def merge_video_audio(video_file_path: str, audio_file_path: str) -> VideoFileCl
 
 
 def save_merged_video(video_clip: VideoFileClip, output_name: str) -> None:
-    print(log.savingMergedVideo)
+    updateLogger(log.savingMergedVideo)
     video_clip.write_videofile(f'{OUTPUT_PATH}/{output_name}.mp4')
     video_clip.close()
     
@@ -89,7 +95,7 @@ def to_srt(
         words: Sequence[pvleopard.Leopard.Word],
         endpoint_sec: float = 1.,
         length_limit: Optional[int] = 16) -> str:
-    print(log.toSRT)
+    updateLogger(log.toSRT)
     def _helper(end: int, ) -> None:
         lines.append("%d" % section)
         lines.append(
@@ -121,7 +127,7 @@ def time_to_seconds(time_obj):
 
 
 def create_subtitle_clips(subtitles, fontsize=18, font='Arial', color='white', debug = False):
-    print(log.subtitleCreate)
+    updateLogger(log.subtitleCreate)
     subtitle_clips = []
 
     for subtitle in subtitles:
@@ -137,41 +143,244 @@ def create_subtitle_clips(subtitles, fontsize=18, font='Arial', color='white', d
         subtitle_clips.append(text_clip.set_position(text_position))
     return subtitle_clips
 
-videoCounter = 0
-posts = get_reddit_posts('AmITheAsshole')
-engine = init_tts_engine()
-for post in posts:
-    if post.selftext and post.selftext != '':
-        videoCounter = videoCounter + 1
-        #print(f'{post.title} : {post.selftext}') #COMMENTED TO SAVE RUNTIME
+def RedditScraperEngine(selectedSubReddit, sliderNum):
+    videoCounter = 0
+    #posts = get_reddit_posts('AmITheAsshole')
+    posts = get_reddit_posts(f'{selectedSubReddit}', sliderNum)
+    engine = init_tts_engine()
+    for post in posts:
+        if post.selftext and post.selftext != '':
+            videoCounter = videoCounter + 1
+            #print(f'{post.title} : {post.selftext}') #COMMENTED TO SAVE RUNTIME
+            
+            output_name = re.sub('[^A-Za-z0-9]+', '', {post.title}.__str__())
+            engine.save_to_file(post.selftext, f'{TEMP_PATH}/{output_name}.mp3')
+            
+            # The minecraft.mp4 is the name of a video I saved to my downloads path to use as the video base.
+            audioVideoOutput = merge_video_audio(f'{SAVE_PATH}/minecraft.mp4','/Users/alexbrady/Library/Mobile Documents/com~apple~CloudDocs/RedditScrape Repo/reddit-scraper-to-tiktok/static/downloads/AITAfor.mp3')
+            
+            transcript, words = leopard.process_file('/Users/alexbrady/Library/Mobile Documents/com~apple~CloudDocs/RedditScrape Repo/reddit-scraper-to-tiktok/static/downloads/AITAfor.mp3')
+            
+            with open(f'{TEMP_PATH}/{output_name}.srt', 'w') as f:
+                f.write(to_srt(words))  #CREATES SRT FROM TEXT
+            
+            subtitles = pysrt.open(f'{TEMP_PATH}/{output_name}.srt') #OPENS SRT FILE FOR READING
+            subtitle_clips = create_subtitle_clips(subtitles) #FORMS MP4 WITH SUBTITLES                            
+            
+            updateLogger(log.buildingVideo)
+            subtitleFinal = CompositeVideoClip([audioVideoOutput] + subtitle_clips) #COMBINES SRT WITH MP4
+            
+            updateLogger(log.writingVideo)
+            subtitleFinal.write_videofile(f'{OUTPUT_PATH}/{output_name}.mp4') #WRITES AND SAVES TO OUTPUTPATH
+            #save_merged_video(subtitleFinal, output_name=output_name)
+            
+            updateVideoCounter(videoCounter)
+            if (videoCounter == 1):
+                print(f'{videoCounter} VIDEO MADE')
+            else:
+                print(f'{videoCounter} VIDEOS MADE')
         
-        output_name = re.sub('[^A-Za-z0-9]+', '', {post.title}.__str__())
-        engine.save_to_file(post.selftext, f'{TEMP_PATH}/{output_name}.mp3')
-        
-        # The minecraft.mp4 is the name of a video I saved to my downloads path to use as the video base.
-        audioVideoOutput = merge_video_audio(f'{SAVE_PATH}/minecraft.mp4','/Users/alexbrady/Library/Mobile Documents/com~apple~CloudDocs/RedditScrape Repo/reddit-scraper-to-tiktok/static/downloads/AITAfor.mp3')
-        
-        transcript, words = leopard.process_file('/Users/alexbrady/Library/Mobile Documents/com~apple~CloudDocs/RedditScrape Repo/reddit-scraper-to-tiktok/static/downloads/AITAfor.mp3')
-        
-        with open(f'{TEMP_PATH}/{output_name}.srt', 'w') as f:
-            f.write(to_srt(words))  #CREATES SRT FROM TEXT
-        
-        subtitles = pysrt.open(f'{TEMP_PATH}/{output_name}.srt') #OPENS SRT FILE FOR READING
-        subtitle_clips = create_subtitle_clips(subtitles) #FORMS MP4 WITH SUBTITLES                            
-        
-        subtitleFinal = CompositeVideoClip([audioVideoOutput] + subtitle_clips) #COMBINES SRT WITH MP4
-        subtitleFinal.write_videofile(f'{OUTPUT_PATH}/{output_name}.mp4') #WRITES AND SAVES TO OUTPUTPATH
-        #save_merged_video(subtitleFinal, output_name=output_name)
-        
-        if (videoCounter == 1):
-            print(f'{videoCounter} VIDEO MADE')
         else:
-            print(f'{videoCounter} VIDEOS MADE')
+            print('No Post Body')
+
+
+#methods to take mp4 compiled videos and post to respective platforms UNUSED
+def postTikTik():
+    print()
+def postFacebook():
+    print()
+def postYoutube():
+    print()
+
+# **************** TKINTER WINDOW SETUP / TKINTER METHODS *********************
+window = tk.Tk()
+window.title('Scraping Home')
+window.geometry('700x300')
+
+leftFrameMain = tk.Frame(window)
+leftFrameMain.pack(side='left', fill='both')
+
+rightFrameMain = tk.Frame(window)
+rightFrameMain.pack(side='right', fill='both')
+
+bottomFramMain = tk.Frame(window)
+bottomFramMain.pack(side= 'bottom', fill='y')
+
+
+videocounter = tk.Text(rightFrameMain, height=2, width=15, font=('arial 18'),)
+videocounter.pack()
+
+appLogger = st.ScrolledText(rightFrameMain, height=8, width=100)
+appLogger.pack()
+
+
+def updateLogger(message : str):
+    appLogger.insert(tk.INSERT, message + '\n')
+    appLogger.update()
+    print(message)
     
+def updateVideoCounter(numVids):
+    videocounter.delete("1.0", "end")
+    videocounter.insert('end', f'Videos created: {numVids}')
+    videocounter.update()
+
+def getWeb(url):
+    webbrowser.open_new(url)
+
+
+gitLink = tk.Label(leftFrameMain, text='GitHub Repo', font='Helvetica 15 underline', fg='light blue')
+gitLink.pack()
+gitLink.bind("<Button-1>", lambda e:(getWeb('https://github.com/dotlonely/reddit-scraper-to-tiktok')))
+
+redditLink = tk.Label(leftFrameMain, text='Reddit Page', font='Helvetica 15 underline', fg='light blue')
+redditLink.pack()
+redditLink.bind("<Button-2>", lambda e:(getWeb('https://www.reddit.com')))
+
+def newTikTokWindow():
+    tiktokWindow = tk.Toplevel(window)
+    tiktokWindow.title('TikTok from Reddit')
+    tiktokWindow.geometry('600x300')
+
+    subRedditLabel = tk.Label(tiktokWindow,text='Choose SubReddit:',font=(14)).grid(row=0)
+    subReddits = [
+        'AmITheAsshole',
+        'Money',
+        'LegalAdvice',
+        'Scams'
+                ]
+
+    clicked = tk.StringVar() 
+    clicked.set( "AmITheAsshole") 
+
+    drop = tk.OptionMenu( tiktokWindow , clicked , *subReddits ) 
+    drop.grid(row=0,column=1)
+    drop.config(width=14)
+
+    redditPostNumSlider = tk.Scale(tiktokWindow, label='Number of Reddit posts to be scraped', orient='horizontal', length=400, width=45, from_=0, to=50, cursor='dot', activebackground='red') 
+    redditPostNumSlider.grid(row=20, column=0, columnspan=10)
+    
+    vidCreateButton = tk.Button( tiktokWindow,command=(lambda:RedditScraperEngine(clicked.get(),redditPostNumSlider.get())), text="begin creating videos",height=1, width=15)
+    vidCreateButton.grid(row=0, column=10)
+
+    postTikTok = tk.IntVar()
+    tiktokPostCheckBox = tk.Checkbutton(tiktokWindow, text='Post to TikTok', variable=postTikTok)
+    tiktokPostCheckBox.grid(row=3, column=10)
+    if postTikTok.get():
+        print() #will post to tiktok
     else:
-        print('No Post Body')
+        print() #will not post to tiktok
+    
+    savemp4 = tk.IntVar()
+    savemp4CheckBox = tk.Checkbutton(tiktokWindow, text='Save mp4 file(s)', variable=savemp4)
+    savemp4CheckBox.grid(row=4, column=10)
+    if savemp4.get():
+        print() #call method to save to computer hard drive ... use save_merged_video(video, hardDrive)
+    else:
+        print() # sends boolean to delete_video method in the engine loop
+
+def newFacebookWindow():
+    facebookWindow = tk.Toplevel(window)
+    facebookWindow.geometry('600x300')
+    facebookWindow.title('Facebook from Reddit')
+    facebookWindow.config(bg='RoyalBlue1')
+    
+    subRedditLabel = tk.Label(facebookWindow,text='Choose SubReddit:',font=(14)).grid(row=0)
+    subReddits = [
+        'AmITheAsshole',
+        'Money',
+        'LegalAdvice',
+        'Scams'
+                ]
+
+    clicked = tk.StringVar() 
+    clicked.set( "AmITheAsshole") 
+
+    drop = tk.OptionMenu( facebookWindow , clicked , *subReddits ) 
+    drop.grid(row=0,column=1)
+    drop.config(width=14)
+
+    redditPostNumSlider = tk.Scale(facebookWindow, label='Number of Reddit posts to be scraped', orient='horizontal', length=400, width=45, from_=0, to=50, cursor='dot', activebackground='red') 
+    redditPostNumSlider.grid(row=20, column=0, columnspan=10)
+    
+    vidCreateButton = tk.Button( facebookWindow, text="begin creating videos",height=1, width=15)
+    vidCreateButton.grid(row=0, column=10)
+
+    postTikTok = tk.IntVar()
+    facebookPostCheckBox = tk.Checkbutton(facebookWindow, text='Post to Facebook', variable=postTikTok)
+    facebookPostCheckBox.grid(row=3, column=10)
+    if postTikTok.get():
+        print() #will post to tiktok
+    else:
+        print() #will not post to tiktok
+    
+    savemp4 = tk.IntVar()
+    savemp4CheckBox = tk.Checkbutton(facebookWindow, text='Save mp4 file(s)', variable=savemp4)
+    savemp4CheckBox.grid(row=4, column=10)
+    if savemp4.get():
+        print() #call method to save to computer hard drive ... use save_merged_video(video, hardDrive)
+    else:
+        print() # sends boolean to delete_video method in the engine loop
+    
+
+def newYoutubeWindow():
+    youtubeWindow = tk.Toplevel(window)
+    youtubeWindow.geometry('600x300')
+    youtubeWindow.title('Youtube from Reddit')
+    
+    subRedditLabel = tk.Label(youtubeWindow,text='Choose SubReddit:',font=(14)).grid(row=0)
+    subReddits = [
+        'AmITheAsshole',
+        'Money',
+        'LegalAdvice',
+        'Scams'
+                ]
+
+    clicked = tk.StringVar() 
+    clicked.set( "AmITheAsshole") 
+
+    drop = tk.OptionMenu( youtubeWindow , clicked , *subReddits ) 
+    drop.grid(row=0,column=1)
+    drop.config(width=14)
+
+    redditPostNumSlider = tk.Scale(youtubeWindow, label='Number of Reddit posts to be scraped', orient='horizontal', length=400, width=45, from_=0, to=50, cursor='dot', activebackground='red') 
+    redditPostNumSlider.grid(row=20, column=0, columnspan=10)
+    
+    vidCreateButton = tk.Button( youtubeWindow, text="begin creating videos",height=1, width=15)
+    vidCreateButton.grid(row=0, column=10)
+
+    postTikTok = tk.IntVar()
+    youtubePostCheckBox = tk.Checkbutton(youtubeWindow, text='Post to Youtube', variable=postTikTok)
+    youtubePostCheckBox.grid(row=3, column=10)
+    if postTikTok.get():
+        print() #will post to tiktok
+    else:
+        print() #will not post to tiktok
+    
+    savemp4 = tk.IntVar()
+    savemp4CheckBox = tk.Checkbutton(youtubeWindow, text='Save mp4 file(s)', variable=savemp4)
+    savemp4CheckBox.grid(row=4, column=10)
+    if savemp4.get():
+        print() #call method to save to computer hard drive ... use save_merged_video(video, hardDrive)
+    else:
+        print() # sends boolean to delete_video method in the engine loop
+
+#menu bar
+mainMenu = tk.Menu(window)
+appMenu = tk.Menu(mainMenu, tearoff=0)
+appMenu.add_command(label='TikTok', command=newTikTokWindow)
+appMenu.add_command(label='Facebook', command=newFacebookWindow)
+appMenu.add_command(label='Youtube', command=newYoutubeWindow)
+appMenu.add_separator()
+appMenu.add_command(label='Quit', command=window.quit)
+mainMenu.add_cascade(label='Reddit', menu=appMenu)
+window.config(menu=mainMenu)
 
 
 
 
 
+
+
+window.mainloop()
+
+# ********************************* END OF WINDOW SETUP ***********************************************
